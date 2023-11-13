2e60a6b1157bc94600945778ff4d71c64dac81a7
Edsu <edsoneurides0705@gmail.com>
2023-11-12 23:17:30 -0300

    Criação de modelos

---
+++ ParkingAPI/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingAPI.Models
+{
+    public class Company
+    {
+        [Key]
+        public long id { get; set; }
+        public string name { get; set; }
+        public string cnpj { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public int qtyMotorcycleSpaces { get; set; }
+        public int qtyCarsSpaces { get; set; }
+    }
+}
---
+++ ParkingAPI/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using ParkingAPI.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingAPI.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int id { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string color { get; set; }
+        public string plate { get; set; }
+        public VehicleType type { get; set; }
+    }
+}
