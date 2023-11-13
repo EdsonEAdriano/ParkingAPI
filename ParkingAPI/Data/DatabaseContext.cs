9176b0793e5a7e8308a4518f29799843054701a3
Edsu <edsoneurides0705@gmail.com>
2023-11-12 23:57:45 -0300

    Contexto do banco

---
+++ ParkingAPI/Data/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingAPI.Models;
+
+namespace ParkingAPI.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
