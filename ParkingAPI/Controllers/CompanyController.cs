a64f9dbadcb90c4f11b48cab8a205dc7f4211d56
Edsu <edsoneurides0705@gmail.com>
2023-11-15 12:37:39 -0300

    controladores de empresa e veículo

---
+++ ParkingAPI/Controllers/CompanyController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingAPI.Models;
+using ParkingAPI.Repositories;
+
+namespace ParkingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+        private readonly ILogger<CompanyController> _logger;
+
+        public CompanyController(ICompanyRepository companyRepository, ILogger<CompanyController> logger)
+        {
+            _companyRepository = companyRepository;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Company company)
+        {
+            try
+            {
+                await _companyRepository.add(company);
+                _logger.LogTrace("Empresa adicionada.");
+                return Ok(company);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while adding new company. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest(company);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var companies = await _companyRepository.get();
+                return Ok(companies);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while getting company. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while getting company. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var company = await _companyRepository.get(id);
+                return Ok(company);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while getting company. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while getting company. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Company company)
+        {
+            try
+            {
+                var companyEdit = await _companyRepository.edit(company);
+                return Ok(companyEdit);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while updating company. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while updating company. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var company = await _companyRepository.remove(id);
+                return Ok(company);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while removing company. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while removing company. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+    }
+}
---
+++ ParkingAPI/Controllers/VehicleController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingAPI.Models;
+using ParkingAPI.Repositories;
+
+namespace ParkingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IVehicleRepository _vehicleRepository;
+        private readonly ILogger<VehicleController> _logger;
+
+        public VehicleController(IVehicleRepository vehicleRepository, ILogger<VehicleController> logger)
+        {
+            _vehicleRepository = vehicleRepository;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Vehicle vehicle)
+        {
+            try
+            {
+                await _vehicleRepository.add(vehicle);
+                _logger.LogTrace("Veículo adicionado.");
+                return Ok(vehicle);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while adding new vehicle. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest(vehicle);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var companies = await _vehicleRepository.get();
+                return Ok(companies);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while getting vehicle. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while getting vehicle. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var vehicle = await _vehicleRepository.get(id);
+                return Ok(vehicle);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while getting vehicle. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while getting vehicle. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Vehicle vehicle)
+        {
+            try
+            {
+                var companyEdit = await _vehicleRepository.edit(vehicle);
+                return Ok(companyEdit);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while updating vehicle. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while updating vehicle. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var vehicle = await _vehicleRepository.remove(id);
+                return Ok(vehicle);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while removing vehicle. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest($"Error while removing vehicle. ERROR MESSAGE: {e.Message}; ");
+            }
+        }
+    }
+}
