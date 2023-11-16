027cd8c9a8e46b4712736235b76654aa3acab475
Edsu <edsoneurides0705@gmail.com>
2023-11-15 21:30:55 -0300

    Criação de modelo de controle

---
+++ ParkingAPI/Models/Control.cs
@@ -0,0 +1,20 @@
+﻿using ParkingAPI.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingAPI.Models
+{
+    public class Control
+    {
+        [Key]
+        public long id { get; set; }
+        [ForeignKey("company")]
+        public long companyID { get; set; }
+        public Company company { get; set; }
+        [ForeignKey("vehicle")]
+        public int vehicleID { get; set; }
+        public Vehicle vehicle { get; set; }
+        public ControlType type { get; set; }
+        public DateTime createDate { get; set; } = DateTime.Now;
+    }
+}
