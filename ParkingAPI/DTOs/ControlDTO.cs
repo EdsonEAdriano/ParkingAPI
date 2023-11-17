a9ebacab090931b94d9db8442245b89d1c08215b
Edsu <edsoneurides0705@gmail.com>
2023-11-16 21:22:26 -0300

    Criação DTO de Controle

---
+++ ParkingAPI/DTOs/ControlDTO.cs
@@ -0,0 +1,12 @@
+﻿using ParkingAPI.Enums;
+using ParkingAPI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkingAPI.DTOs
+{
+    public class ControlDTO
+    {
+        public long companyID { get; set; }
+        public int vehicleID { get; set; }
+    }
+}
