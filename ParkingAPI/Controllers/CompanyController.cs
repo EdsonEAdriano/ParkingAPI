8b6977fe2aec4c73d1a6f913b0836a85ffcd09b2
Edsu <edsoneurides0705@gmail.com>
2023-11-18 22:05:56 -0300

    Validações de CNPJ em no controlador de empresas

--- ParkingAPI/Controllers/CompanyController.cs
+++ ParkingAPI/Controllers/CompanyController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ParkingAPI.DTOs;
+using ParkingAPI.Helpers;
 using ParkingAPI.Models;
 using ParkingAPI.Repositories;
 
@@ -18,18 +20,35 @@ namespace ParkingAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] Company company)
+        public async Task<IActionResult> Post([FromBody] CompanyDTO companyDTO)
         {
             try
             {
-                await _companyRepository.add(company);
-                _logger.LogTrace("Empresa adicionada.");
-                return Ok(company);
+                if (!CNPJ.validateCNPJ(companyDTO.cnpj))
+                {
+                    _logger.LogError($"Error while adding new company. ERROR MESSAGE: CNPJ inválido; ");
+                    return BadRequest("CNPJ inválido");
+                }
+
+                Company company = new Company(companyDTO);
+
+                if (!await _companyRepository.existsCNPJ(company.cnpj))
+                {
+                    await _companyRepository.add(company);
+                    _logger.LogTrace("Empresa adicionada.");
+                    return Ok(company);
+                }
+                else
+                {
+                    _logger.LogError($"Error while adding new company. ERROR MESSAGE: CNPJ already exists; ");
+                    return BadRequest("CNPJ já existe");
+                }
+                
             }
             catch (Exception e)
             {
                 _logger.LogError($"Error while adding new company. ERROR MESSAGE: {e.Message}; ");
-                return BadRequest(company);
+                return BadRequest(companyDTO);
             }
         }
 
@@ -65,12 +84,28 @@ namespace ParkingAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<IActionResult> Put([FromBody] Company company)
+        public async Task<IActionResult> Put(int id, [FromBody] CompanyDTO companyDTO)
         {
             try
             {
-                var companyEdit = await _companyRepository.edit(company);
-                return Ok(companyEdit);
+                if (!CNPJ.validateCNPJ(companyDTO.cnpj))
+                {
+                    _logger.LogError($"Error while adding new company. ERROR MESSAGE: CNPJ inválido; ");
+                    return BadRequest("CNPJ inválido");
+                }
+
+                Company company = new Company(id, companyDTO);
+
+                if (!await _companyRepository.existsCNPJ(company.cnpj))
+                {
+                    await _companyRepository.edit(company);
+                    return Ok(companyDTO);
+                }
+                else
+                {
+                    _logger.LogError($"Error while adding new company. ERROR MESSAGE: CNPJ already exists; ");
+                    return BadRequest("CNPJ já existe");
+                }
             }
             catch (Exception e)
             {
