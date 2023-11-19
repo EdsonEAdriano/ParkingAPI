3dcbb85706afd49102beafae054dbd816675dae8
Edsu <edsoneurides0705@gmail.com>
2023-11-18 22:05:04 -0300

    Alteração para long o cnpj

---
+++ ParkingAPI/Migrations/20231117020009_alterTableCompany.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class alterTableCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "cnpj",
+                table: "Companies",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "cnpj",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
--- ParkingAPI/Models/Company.cs
+++ ParkingAPI/Models/Company.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ParkingAPI.DTOs;
+using ParkingAPI.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace ParkingAPI.Models
 {
@@ -7,10 +9,35 @@ namespace ParkingAPI.Models
         [Key]
         public long id { get; set; }
         public string name { get; set; }
-        public string cnpj { get; set; }
+        public long cnpj { get; set; }
         public string address { get; set; }
         public string phone { get; set; }
         public int qtyMotorcycleSpaces { get; set; }
         public int qtyCarsSpaces { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(CompanyDTO companyDTO)
+        {
+            this.name = companyDTO.name;
+            this.cnpj = CNPJ.parseCNPJ(companyDTO.cnpj);
+            this.address = companyDTO.address;
+            this.phone = companyDTO.phone;
+            this.qtyMotorcycleSpaces = companyDTO.qtyMotorcycleSpaces;
+            this.qtyCarsSpaces = companyDTO.qtyCarsSpaces;
+        }
+
+        public Company(int id, CompanyDTO companyDTO)
+        {
+            this.id = id;
+            this.name = companyDTO.name;
+            this.cnpj = CNPJ.parseCNPJ(companyDTO.cnpj);
+            this.address = companyDTO.address;
+            this.phone = companyDTO.phone;
+            this.qtyMotorcycleSpaces = companyDTO.qtyMotorcycleSpaces;
+            this.qtyCarsSpaces = companyDTO.qtyCarsSpaces;
+        }
     }
 }
