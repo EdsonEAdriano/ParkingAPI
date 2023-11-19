b6c2518662161c8d1e022092ad25c310a45cd166
Edsu <edsoneurides0705@gmail.com>
2023-11-18 22:06:25 -0300

    Controle de erros no controlador de Controle

--- ParkingAPI/Controllers/ControlController.cs
+++ ParkingAPI/Controllers/ControlController.cs
@@ -11,51 +11,139 @@ namespace ParkingAPI.Controllers
     public class ControlController : ControllerBase
     {
         private readonly IControlRepository _controlRepository;
+        private readonly ICompanyRepository _companyRepository;
+        private readonly IVehicleRepository _vehicleRepository;
+        private readonly ILogger<ControlController> _logger;
 
-        public ControlController(IControlRepository controlRepository) 
+        public ControlController(IControlRepository controlRepository, ILogger<ControlController> logger, ICompanyRepository companyRepository, IVehicleRepository vehicleRepository) 
         {
             _controlRepository = controlRepository;
+            _companyRepository = companyRepository;
+            _vehicleRepository = vehicleRepository;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var controls = await _controlRepository.get();
+            try
+            {
+                var controls = await _controlRepository.get();
 
-            return Ok(controls);
+                return Ok(controls);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while getting control. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest("Erro ao coletar controles");
+            }         
         }
 
         [HttpGet]
         [Route("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            var control =  await _controlRepository.get(id);
+            try
+            {
+                var control = await _controlRepository.get(id);
 
-            return Ok(control);
+                return Ok(control);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while getting control. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest("Erro ao coletar controles");
+            }
+            
         }
 
         [HttpPost]
         public async Task<IActionResult> Add(ControlDTO controlDTO)
         {
-            Control control = await _controlRepository.add(controlDTO);
+            try
+            {
+                if (!await _companyRepository.exists(controlDTO.companyID))
+                {
+                    _logger.LogError($"Error while adding control. ERROR MESSAGE: Vehicle do not exists; ");
+                    return BadRequest("Veículo não existe");
+                }
+                else if (!await _vehicleRepository.exists(controlDTO.vehicleID))
+                {
+                    _logger.LogError($"Error while adding control. ERROR MESSAGE: Company do not exists; ");
+                    return BadRequest("Empresa não existe");
+                }
 
-            return Ok(control);
+                if (!await _controlRepository.isParkedByDTO(controlDTO))
+                {
+                    Control control = await _controlRepository.add(controlDTO);
+
+                    return Ok(control);
+                }
+                else
+                {
+                    _logger.LogError($"Error while adding control. ERROR MESSAGE: Vehicle is parked; ");
+                    return BadRequest("Veículo já esta estacionado");
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while adding control. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest("Erro ao adicionar controle");
+            }
         }
 
         [HttpPut]
         [Route("{id}")]
         public async Task<IActionResult> Exit(int id)
         {
-            var control = await _controlRepository.exit(id);
-            return Ok(control);
+            try
+            {
+                if (await _controlRepository.isParked(id))
+                {
+                    var control = await _controlRepository.exit(id);
+                    return Ok(control);
+                }
+                else
+                {
+                    _logger.LogError($"Error while updating control. ERROR MESSAGE: Vehicle is not parked; ");
+                    return BadRequest("Veículo não esta estacionado");
+                }
+            }
+            catch (Exception e )
+            {
+                _logger.LogError($"Error while updating control. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest("Erro ao atualizar controle");
+            }
+            
+            
         }
 
         [HttpDelete]
         [Route("{id}")]
         public async Task<IActionResult> Remove(int id)
         {
-            var control = await _controlRepository.remove(id);
-            return Ok(control);
+            try
+            {
+                if (await _controlRepository.exists(id))
+                {
+                    var control = await _controlRepository.remove(id);
+                    return Ok(control);
+                }
+                else
+                {
+                    _logger.LogError($"Error while removing control. ERROR MESSAGE: Control do not exists; ");
+                    return BadRequest("Controle não existe");
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Error while removing control. ERROR MESSAGE: {e.Message}; ");
+                return BadRequest("Erro ao remover controle");
+            }
+            
+            
         }
     }
 }
