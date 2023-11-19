1939e04633d0a6730cde06d188b609786f08b957
Edsu <edsoneurides0705@gmail.com>
2023-11-18 22:01:50 -0300

    Criação de DTOs para Empresa e Veículo

---
+++ ParkingAPI/DTOs/CompanyDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkingAPI.DTOs
+{
+    public class CompanyDTO
+    {
+        public string name { get; set; }
+        public string cnpj { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public int qtyMotorcycleSpaces { get; set; }
+        public int qtyCarsSpaces { get; set; }
+    }
+}
---
+++ ParkingAPI/DTOs/VehicleDTO.cs
@@ -0,0 +1,13 @@
+﻿using ParkingAPI.Enums;
+
+namespace ParkingAPI.DTOs
+{
+    public class VehicleDTO
+    {
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string color { get; set; }
+        public string plate { get; set; }
+        public VehicleType type { get; set; }
+    }
+}
