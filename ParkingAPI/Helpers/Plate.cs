bc180a9d3344d93e1c3c1a948b1b13091e35ef86
Edsu <edsoneurides0705@gmail.com>
2023-11-19 01:07:21 -0300

    Validação de placas

---
+++ ParkingAPI/Helpers/Plate.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ParkingAPI.Helpers
+{
+    public static class Plate
+    {
+        public static bool validatePlate(string placa)
+        {
+            string pattern = @"^[A-Za-z]{3}-\d{4}$|^[A-Za-z]{3}\d[A-Za-z]\d{2}$";
+            Regex regex = new Regex(pattern);
+
+            return regex.IsMatch(placa);
+        }
+    }
+}
