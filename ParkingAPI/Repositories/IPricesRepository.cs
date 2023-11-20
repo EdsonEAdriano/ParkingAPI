82da8eda6ba9aa5aea389edf95500789be21bd31
Edsu <edsoneurides0705@gmail.com>
2023-11-20 00:20:40 -0300

    Criação do repositório de preços

---
+++ ParkingAPI/Repositories/IPricesRepository.cs
@@ -0,0 +1,13 @@
+﻿using ParkingAPI.Models;
+
+namespace ParkingAPI.Repositories
+{
+    public interface IPricesRepository
+    {
+        public Task<Prices> add(Prices price);
+        public Task<List<Prices>> get();
+        public Task<Prices> get(short id);
+        public Task<Prices> update(Prices price);
+        public Task<Prices> delete(Prices price);
+    }
+}
---
+++ ParkingAPI/Repositories/PricesRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingAPI.Data;
+using ParkingAPI.Models;
+
+namespace ParkingAPI.Repositories
+{
+    public class PricesRepository : IPricesRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public PricesRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Prices> add(Prices price)
+        {
+            await _context.Prices.AddAsync(price);
+            await _context.SaveChangesAsync();
+            return price;
+        }
+
+        public async Task<Prices> delete(Prices price)
+        {
+            _context.Prices.Remove(price);
+            await _context.SaveChangesAsync();
+            return price;
+        }
+
+        public async Task<List<Prices>> get()
+        {
+            return await _context.Prices.ToListAsync();
+        }
+
+        public async Task<Prices> get(short id)
+        {
+            return await _context.Prices.FirstOrDefaultAsync(p => p.id == id);
+        }
+
+        public async Task<Prices> update(Prices price)
+        {
+            _context.Prices.Update(price);
+            await _context.SaveChangesAsync();
+            return price;
+        }
+    }
+}
