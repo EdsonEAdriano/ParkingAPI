c768eeb5c62ab29f36e97d46c8811a88c2bc029b
Edsu <edsoneurides0705@gmail.com>
2023-11-20 00:19:56 -0300

    Criação modelo de preços

---
+++ ParkingAPI/Models/Prices.cs
@@ -0,0 +1,30 @@
+﻿using ParkingAPI.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingAPI.Models
+{
+    public class Prices
+    {
+        [Key]
+        public short id { get; set; }
+        public short hours { get; set; }
+        public double price { get; set; }
+
+        public Prices()
+        {
+        }
+
+        public Prices(PricesDTO pricesDTO)
+        {
+            this.hours = pricesDTO.hours;
+            this.price = pricesDTO.price;
+        }
+
+        public Prices(short id, PricesDTO pricesDTO)
+        {
+            this.id = id;
+            this.hours = pricesDTO.hours;
+            this.price = pricesDTO.price;
+        }
+    }
+}
